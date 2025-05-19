31429ade6727da2778ef5a99dc9a48376b92b88c
Varo Melkonyan <58566427+varo-melkonyan@users.noreply.github.com>
2025-05-19 18:38:10 +0400

    Update server.js

--- server.js
+++ server.js
@@ -124,7 +124,7 @@ mongoose.connect(MONGODB_URI, {
   .then(() => {
     console.log('✅ MongoDB connected');
 
-    const PORT = process.env.PORT || 5001;
+    const PORT = process.env.PORT || 10000;
     server.listen(PORT, () => {
       console.log(`🚀 Server running on http://localhost:${PORT}`);
     });
