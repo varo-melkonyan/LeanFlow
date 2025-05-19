f82f5840f4f18f8930eb01a35306b1cecbfa22e0
Varo Melkonyan <58566427+varo-melkonyan@users.noreply.github.com>
2025-05-20 01:03:55 +0400

    Update server.js

--- server.js
+++ server.js
@@ -46,6 +46,7 @@ app.use('/api/comments', commentRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/tickets', ticketRoutes);
 app.use('/api/messages', messageRoutes);
+app.use('/api/auth', require('./routes/auth'));
 
 
 const server = http.createServer(app);
